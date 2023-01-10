395b7464ed519b6f059659280c9f18b87c036c0f
JulianoKosloski <juliano.koslos@gmail.com>
2023-01-10 16:52:30 -0300

    more bash

--- hello.sh
+++ hello.sh
@@ -10,12 +10,9 @@ echo "Hello, $name! How are you doing today?"
 echo "If you are fine, type [1], else type [2]:"
 read status
 
-if [status -eq 1]
+if [[ $status -eq 1 ]] #whitespace between brackets and variables is very important
 then
     echo "That's nice to hear!"
-fi
-#-eq does a numerical comparison, while = does a string comparison
-if [status -eq 2]
-then
-    echo "Oh, I'm sorry to hear that."
+else
+    echo "Sorry to hear that."
 fi
